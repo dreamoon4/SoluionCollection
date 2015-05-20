2f1d9faef5379307a4e6fcb902d86145baf60d0d
tmt514 <math.tmt514@gmail.com>
2015-05-20 09:01:00 +0000

    problems controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,18 +1,29 @@
 class ProblemsController < ApplicationController
+  before_filter :get_problem, :only => [:create, :update]
 
   def index
     @problems = Problem.all
   end
 
   def new
+    @problem = Problem.new
   end
   
   def create
+    Problem.create(@problem)
+    redirect_to problems_path
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  def update
+  end
+
+  def get_problem
+    @problem = params.require(:problem).permit(:title, :unique_name, :description)
   end
 end
