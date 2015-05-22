cfa68aefd42c60999ba05819ee83390ead892beb
Jonathan Hao <johnathan79717@gmail.com>
2015-05-22 13:32:15 +0000

    user info hash: getter and setter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   has_many :solutions
   has_many :user_badges
   has_many :badges ,through: :user_badges
+  has_many :user_infos
+
+  def get key
+    info = UserInfo.find_by user_id: id, category: key
+    return info.value unless info.nil?
+  end
+
+  def set key, value
+    info = UserInfo.find_or_create_by user_id: id, category: key
+    info.value = value
+    info.save!
+  end
+  
 end
