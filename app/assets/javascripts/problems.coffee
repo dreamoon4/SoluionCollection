0e90b0d53f900dc7abd7a2325d867cd32276a7ad
tmt514 <math.tmt514@gmail.com>
2015-05-23 03:13:07 +0000

    fix problem search page

--- app/assets/javascripts/problems.coffee
+++ app/assets/javascripts/problems.coffee
@@ -5,7 +5,7 @@
 class Problem
   @add_problem: (prob) ->
     x = HandlebarsTemplates['problems/tablerow']({ prob: prob })
-    y = $('#search-result-tbody').append(x)
+    y = $('#search-result-segment').append(x)
   @init: (query_string) ->
     Problem.page = 1
     task = {
@@ -21,12 +21,32 @@ class Problem
       if Problem.page < data.page
         Problem.page = data.page
     )
-    dispatcher.bind('problems.next_call', (data) ->
+
+    get_next_pages = ->
       task = {
         search: query_string,
         page: Problem.page + 1
       }
       dispatcher.trigger('problems.search', task)
+
+    dispatcher.bind('problems.next_call', (data) ->
+      if Problem.scroll_ok == true
+        get_next_pages()
+        Problem.scroll_ok = false
+      else
+        Problem.pending_call = true
+    )
+    $(window).scroll( ->
+      if Problem.scroll_ok != true
+        height = $(window).scrollTop()
+        total = $(document).height() - $(window).height()
+        percent = height/total
+        if percent > 0.95 or total - height < $(window).height()
+          if Problem.pending_call == true
+            get_next_pages()
+            Problem.pending_call = false
+          else
+            Problem.scroll_ok = true
     )
     dispatcher.trigger('problems.search', task)
   
--- app/controllers/data_websocket_controller.rb
+++ app/controllers/data_websocket_controller.rb
@@ -1,12 +1,13 @@
 class DataWebsocketController < WebsocketRails::BaseController
   include ApplicationHelper
+  MESSAGE_LIMIT = 5
   def problem_search_results
     q = message.fetch('search', '')
     page = message.fetch('page', 1)
     puts message
     # count = Problem.search_all(q).count
     page_count = 0
-    while page_count < 10 do
+    while page_count < MESSAGE_LIMIT do
       @problems = Problem.search(q, page)
       @problems = @problems.all
       break if @problems.count == 0
@@ -22,7 +23,9 @@ class DataWebsocketController < WebsocketRails::BaseController
         sols = []
         prob.solutions.find_each do |sol|
           sols << {
-            link: solution_path(sol)
+            link: sol.content,
+            rating: sol.rating,
+            path: solution_path(sol)
           }
         end
 
@@ -30,7 +33,7 @@ class DataWebsocketController < WebsocketRails::BaseController
           new_solution_path: new_solution_path(pname: prob.unique_name),
           edit_problem_path: edit_problem_path(prob),
           unique_name: prob.unique_name,
-          problem_link: prob.link? ? prob.description : problem_path(prob),
+          link: prob.link? ? prob.description : problem_path(prob),
           title: prob.title,
           tags: tags,
           solutions: sols
@@ -40,7 +43,7 @@ class DataWebsocketController < WebsocketRails::BaseController
       page = page + 1
       page_count = page_count + 1
     end
-    if page_count >= 10
+    if page_count >= MESSAGE_LIMIT
       send_message :next_call, 'xd', namespace: :problems
     end
   end
